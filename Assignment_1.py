406a0677e36b606b8e64222c6899ba50e4146763
belkiskara <63133974+belkiskara@users.noreply.github.com>
2020-07-07 00:20:40 -0400

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,4 @@
+# Exam1 = input ("Exam point : ")
+# Exam2 = input ("Exam point : ")
+# Average = (float(Exam1))*0.3+(float(Exam2)*0.7)
+# print("Average : {}{}ve{} ".format(Average, "Henry", "Claruswar"))
