846447eae116db3257f46a6b2d606f596a1e7482
belkiskara <63133974+belkiskara@users.noreply.github.com>
2020-07-12 22:34:31 -0400

    Add files via upload

---
+++ Assignment_4.py
@@ -0,0 +1,21 @@
+while True :
+    number = (input("\nPlease Enter the Number to Check for Armstrong: "))
+    sum1=0
+
+    if ("," in number) or ("." in number) :
+        print("Please enter an integer number")
+    elif int(number) < 0:
+        print("Please enter positive number")
+    elif not number.isdigit():
+        print("Do not use any entries other than numeric values")
+    else:
+        for i in range(len(number)):
+         sum1 += int(number[i]) ** len(number)
+        if sum1 == int(number):
+            print(number, "is an Armstrong number")
+            break
+        else:
+            print( number, "is not an Armstrong number")
+            break
+        
+         
