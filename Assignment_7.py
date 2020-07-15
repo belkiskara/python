61a4d14ad651d8471f78d53ba15eea46bf3ae220
belkiskara <63133974+belkiskara@users.noreply.github.com>
2020-07-15 02:16:54 -0400

    Add files via upload

---
+++ Assignment_7.py
@@ -0,0 +1,11 @@
+unchanged_num = []
+for i in range(101):
+ if i%3==0 and i%5==0:
+  print("FizzBuzz")
+ elif i%3==0:
+  print('Fizz')
+ elif i%5==0 :
+  print('Buzz')
+ else:
+  unchanged_num.append(i)  
+print(unchanged_num)
